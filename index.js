f4b086001171a346746bff0a7dc0698334444c37
Taehyeon Ahn <gksdidwnd91@naver.com>
2024-06-01 21:49:52 +0900

    spotify work

--- index.js
+++
@@ -1,9 +0,0 @@
-// 필요한 데이터 각 요소들의 데이터
-// 필요한 기능 - 현재 요소의 스타일 삭제 함수
-
-const buttonGroup = document.querySelectorAll(".btn");
-// 버튼들을 그룹핑하여 노드리스트에 저장함 (각 아이템은 인덱스로 접근함)
-
-for (let i = 0; i < buttonGroup.length; i++) {
-  buttonGroup[i];
-}
